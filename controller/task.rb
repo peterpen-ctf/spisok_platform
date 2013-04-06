97283acf301be504e98f6a710f0c7d646344be1f
Anton Ovchinnikov <revolver112@gmail.com>
2013-04-06 12:53:59 +0400

    Added task update and removal

--- controller/task.rb
+++ controller/task.rb
@@ -6,6 +6,13 @@ class TaskController < Controller
     @current_user = user
   end
 
+  before(:new, :edit, :save, :delete) do
+    if !logged_in? or !@current_user.is_admin
+      flash[:error] = 'You are not an allowed to do that!'
+      redirect r(:all)
+    end
+  end
+
   def index
     redirect r(:all) 
   end
@@ -30,26 +37,65 @@ class TaskController < Controller
 
 
   def new
-    if !logged_in? or !user.is_admin
-      flash[:error] = 'You are not an admin!'
+    @task  = Task.new
+    @submit_label = 'Create task'
+    render_view :edit_task
+  end
+
+  def edit(id)
+    @task = Task[id]
+    if @task.nil?
+      flash[:error] = 'Cannot edit: invalid task!'
       redirect r(:all)
     end
-
-    if request.post?
-      task_name = request[:task_name]
-      task_desc = request[:task_desc]
-
-      task = Task.new(:name => task_name, :description => task_desc)
-      if task.valid?
-        task.save
-        flash[:success] = 'OK'
-        redirect r(:all)
-      else
-        flash[:error] = 'Invalid data!'
-        render_view :new_task
-      end
-    end
-    render_view :new_task
+    @title = 'Edit task'
+    @submit_label = 'Update task'
+    render_view :edit_task
   end
+
+  def delete(id)
+    task = Task[id]
+    if task.nil?
+      flash[:error] = 'Cannot delete: invalid task!'
+    else
+      task.destroy
+    end
+    redirect r(:all)
+  end
+
+  def save
+    redirect r(:all) unless request.post?
+    task_data = request.subset(:name, :description)
+    id = request.params['id']
+
+    # Update task
+    if !id.nil? and !id.empty?
+      task = Task[id]
+      if task.nil?
+        flash[:error] = 'This task is invalid. WTF?'
+        redirect r(:all)
+      end
+
+      success = 'The task has been updated'
+      error = 'The task could not be updated!'
+
+    # New task
+    else
+      task = Task.new
+      success = 'The task has been created'
+      error = 'The task could not be created!'
+    end
+
+
+    begin
+      task.update(task_data)
+      flash[:success] = success
+    rescue => e
+      Ramaze::Log.error(e)
+      flash[:error] = error
+    end
+    redirect r(:all)
+end
+
 
 end

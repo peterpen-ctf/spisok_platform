17be77c6227ad0cc84da194cc665fca803aa7aea
Anton Ovchinnikov <revolver112@gmail.com>
2013-04-12 01:54:53 +0400

    load_tasks script fills the DB from YAML summaries

---
+++ load_tasks.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/env ruby
+
+
+def usage
+  puts <<-USAGE
+Usage:
+  
+  ruby load_tasks.rb TASK_DIR
+  USAGE
+end
+
+path = ARGV[0] || ''
+
+tasks_summary = Dir.glob(path + '/*/summary.yml')
+if tasks_summary.empty?
+  puts 'No tasks found.'
+  usage
+  exit 1
+end
+
+
+app = File.expand_path('../app', __FILE__)
+require app
+require 'yaml'
+
+
+tasks_summary.each do |task_summary|
+  yaml_task = YAML.load_file(task_summary)
+  yaml_task['author'] = yaml_task['category'] = yaml_task['price'] = nil
+  Task.create(yaml_task)
+end
+
+puts "Processed #{tasks_summary.length} tasks."
